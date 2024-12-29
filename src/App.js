414bbf1f7b9ab9766cd2abf59c03e8d400070ed7
Si0dot7 <felm27010@gmail.com>
2024-12-29 17:00:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import {useState} from 'react'
+import Menu from './Components/Menu';
+import Quiz from './Components/Quiz';
+import Score from './Components/Score'
 
 function App() {
+  const [appState,setAppState] = useState('score')
   return (
     <div className="App">
      <h1>Web Development Quiz</h1>
+     {appState === 'menu' && <Menu/>}
+     {appState === 'quiz' && <Quiz/>}
+     {appState === 'score' && <Score/>}
     </div>
   );
 }
---
+++ src/Components/Quiz.js
@@ -0,0 +1,10 @@
+
+const Quiz = () =>{
+    return(
+        <div className = 'quiz'>
+            <h1>Quiz Component</h1>
+        </div>
+    );
+}
+
+export default Quiz
