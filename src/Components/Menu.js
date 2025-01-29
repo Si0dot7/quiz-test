b1717f75d1a3deb578f8ce097bbb6fdaa4b1f22b
Si0dot7 <felm27010@gmail.com>
2025-01-29 18:49:20 +0700

    update

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,5 @@
 import {useContext} from 'react'
-import { DataContext } from '../App';
+import { DataContext } from '../App'
 
 const Menu = () =>{
     const {setAppState} = useContext(DataContext)
